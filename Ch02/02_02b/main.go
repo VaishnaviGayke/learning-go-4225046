086c13130a0efda605175d4605ef16e58ade2acc
VaishnaviGayke <96939816+VaishnaviGayke@users.noreply.github.com>
2025-03-21 19:49:36 +0000

    updated ch02

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,17 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	aNumber := 42
 
-	fmt.Println("Hello from Go!")
+	fmt.Println(str1, str2, str3)
+	StringLen, err := fmt.Println("The value is", aNumber)
+	if err == nil {
+		fmt.Println("String length:", StringLen)
+	}
+	fmt.Printf("Value of aNumber: %v \n", aNumber)
+
+	fmt.Printf("Data Type of aNumber: %T \n", aNumber)
 }
