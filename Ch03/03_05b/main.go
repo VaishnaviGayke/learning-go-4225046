dc689b404904fc78ae0600c38bc3d8cad0a6b565
VaishnaviGayke <96939816+VaishnaviGayke@users.noreply.github.com>
2025-03-22 01:15:00 +0000

    Updated full Ch03

--- Ch03/03_05b/main.go
+++ Ch03/03_05b/main.go
@@ -2,8 +2,43 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	fmt.Println("Maps")
+	states := make(map[string]string)
+	states["WA"] = "Washington"
+	states["NY"] = "New York"
+	states["CA"] = "California"
+	fmt.Println(states)
+
+	// calii := states["CA"]
+	fmt.Println(states["CA"])
+
+	delete(states, "WA")
+	fmt.Println(states)
+
+	states["TX"] = "Texas"
+	fmt.Println(states)
+
+	for k, v := range states {
+		fmt.Printf("%v: %v\n", k,v)
+	}
+
+	keys := make([]string, len(states))
+	i :=0
+	for k := range states{
+		keys[i] = k
+		i++
+	}
+
+	fmt.Println(keys)
+
+	sort.Strings(keys)
+	fmt.Println("sorted keys slice")
+	fmt.Println(keys)
+
+	for i := range keys {
+		fmt.Println(states[keys[i]])
+	}
 }
