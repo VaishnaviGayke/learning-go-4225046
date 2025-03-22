c8788a95898175193fc19735e995d98644c5edc8
VaishnaviGayke <96939816+VaishnaviGayke@users.noreply.github.com>
2025-03-22 05:19:22 +0000

    updated Ch06 code commit

--- Ch06/06_01b/main.go
+++ Ch06/06_01b/main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
+	
 )
 
 func main() {
-	fmt.Println("Files")
+	filename := "./fromString.txt"
+	file, errr := os.Create(filename)
+	defer file.Close()
+	
+	checkError(errr)
+
+	length, errr := io.WriteString(file, "Hello from GO!!!")
+	fmt.Printf("Wrote a file with %v characters\n", length)
+	readFile(filename)
+}
+
+func checkError(errr error){
+	if errr != nil {
+		panic(errr)
+	}
+}
+
+func readFile(filename string){
+	data, errr := os.ReadFile(filename)
+	checkError(errr)
+	fmt.Println("Text read from readFile function:\n", string(data))
 }
--- Ch06/06_02b/main.go
+++ Ch06/06_02b/main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
 	fmt.Println("Network requests")
+	client := http.Client{}
+	req, errr := http.NewRequest("GET", url, nil)
+	checkError(errr)
+
+	req.Header.Set("User-Agent", "")
+	resp, errrr := client.Do(req)
+	checkError(errrr)
+	defer resp.Body.Close()
+
+	fmt.Printf("Response Type:%T \n", resp)
+	bytes, err := io.ReadAll(resp.Body)
+	checkError(err)
+	content := string(bytes)
+	fmt.Print(content)
+
 }
 
-// func checkError(err error) {
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
