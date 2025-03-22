aca9a79aac0f982e2350f8358c134be72afcd460
VaishnaviGayke <96939816+VaishnaviGayke@users.noreply.github.com>
2025-03-22 01:40:42 +0000

    Updated half Ch04

--- Ch04/04_01b/main.go
+++ Ch04/04_01b/main.go
@@ -5,5 +5,16 @@ import (
 )
 
 func main() {
-	fmt.Println("Conditional logic")
+	// theAns := 00
+	var result string
+
+	if theAns := 42; theAns <0 {
+		result = "Less than zero"	
+	}else if theAns == 0 {
+		result = "Equal to zero"
+	}else{
+		result = "Greater than zero"
+	}
+
+	fmt.Println(result)
 }
--- Ch04/04_02b/main.go
+++ Ch04/04_02b/main.go
@@ -10,7 +10,38 @@ func main() {
 	weekday := time.Now().Weekday()
 	fmt.Printf("Today is %v\n", weekday)
 
-	dayNumber := int(weekday)
+	dayNumber := uint(weekday)
 	fmt.Printf("The day as a number is %v\n", dayNumber)
+
+	var result string
+	
+	switch dayNumber = 2; dayNumber{
+	case 1: 
+		result = "It's a Monday"
+	case 2: 
+		result = "It's a Tuesday"
+	case 3: 
+		result = "It's a Wednesday"
+	case 4: 
+		result = "It's a Thursday"
+	case 5: 
+		result = "It's a Friday"
+	default : 
+		result = "It's the Weekend"
+	}
+	fmt.Println(result)
+
+
+	x:= 0
+	switch{
+	case x<0:
+		result = "Less than Zero"
+	case x==0:
+		result = "Equal to Zero"
+	default:
+		result = "Greater than Zero"
+	}
+	fmt.Println(result)
+
 
 }
