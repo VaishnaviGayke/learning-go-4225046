5e4ef34e0b58eb09383bb91ff604045ad0f9bee0
VaishnaviGayke <96939816+VaishnaviGayke@users.noreply.github.com>
2025-03-22 03:59:57 +0000

    Updated Ch05 code

--- Ch05/05_01b/main.go
+++ Ch05/05_01b/main.go
@@ -5,5 +5,33 @@ import (
 )
 
 func main() {
-	fmt.Println("Functions")
+
+	doSomething()
+}
+func doSomething() {
+	fmt.Println("Doing Something")
+	val1 := 3
+	val2 := 5
+	val3 := 6
+	// sum := addValues(val1, val2)
+	sum, count, avg := addAllValues(val1, val2, val3)
+
+	// fmt.Printf("the sum of %v and %v id: %v\n", val1, val2, sum)
+	fmt.Printf("the sum is: %v \n", sum)
+	fmt.Printf("the count is: %v \n", count)
+	fmt.Printf("the average is: %.2f \n", avg)
+}
+
+func addValues(value1, value2 int) int {
+	return value1 + value2
+}
+
+func addAllValues(values ...int) (int, int, float64) {
+	sum := 0 
+	for _, v := range values {
+		sum += v
+	}
+	count := len(values)
+	avg := float64(sum) / float64(count)
+	return sum, count, avg
 }
--- Ch05/05_03b/main.go
+++ Ch05/05_03b/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Println("Goroutines")
+	go say("Hello from the Goroutine")
+	fmt.Println("Hello from main!")
+
+	go func(message string) {
+		fmt.Println(message)
+
+	}("Hello from The anonymous function!!!")
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("All Done!!")
+}
+
+func say(message string) {
+	time.Sleep(1 * time.Second)
+	fmt.Println(message)
 }
